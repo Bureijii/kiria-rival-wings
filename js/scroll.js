d408d2d62ee4e06247a8ec4f8b9aecfbd5342ec6
Bureijii <taegette@hotmail.com>
2024-02-27 22:05:06 -0700

    scroll test 3

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(e) {   
-    if($(window).scrollTop() >=17293) {
-       $(window).scrollTop(17293);
+    if($(window).scrollTop() >=17193) {
+       $(window).scrollTop(17193);
     }
 });
